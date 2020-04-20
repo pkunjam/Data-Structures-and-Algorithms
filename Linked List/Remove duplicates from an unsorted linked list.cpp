725a83b72912dfd5e3be30db393914e55fa251dd
pkunjam <punit17100@iiitnr.edu.in>
2020-04-20 23:39:39 +0530

    duplicates in unsorted

---
+++ Linked List/Remove duplicates from an unsorted linked list.cpp
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+Node* removeDuplicates(Node *root);
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+// Function to remove duplicates from the linked list
+// root: root of the linked list
+Node * removeDuplicates( Node *head) 
+{
+    unordered_set<int> s;
+    Node* curr = head;
+    Node* temp = NULL;
+    
+    while(curr!=NULL)
+    {
+        if(s.find(curr->data)==s.end())
+        {
+            s.insert(curr->data);
+            temp = curr;
+            curr = curr->next;
+        }
+        
+        else
+        {
+            temp->next = temp->next->next;
+            curr = temp->next;
+        }
+    }
+    
+    return head;
+}
