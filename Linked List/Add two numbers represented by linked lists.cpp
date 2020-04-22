a53ddb120ebefbf6834c2e5a25508df0413e1b9d
pkunjam <punit17100@iiitnr.edu.in>
2020-04-22 20:00:50 +0530

    add two numbers

---
+++ Linked List/Add two numbers represented by linked lists.cpp
@@ -0,0 +1,89 @@
+// { Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node* addTwoLists(struct Node* first, struct Node* second);
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_Node = new Node(new_data);
+    new_Node->next = (*head_ref);
+    (*head_ref) = new_Node;
+}
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+void freeList(struct Node* Node) {
+    struct Node* temp;
+    while (Node != NULL) {
+        temp = Node;
+        Node = Node->next;
+        free(temp);
+    }
+}
+int main(void) {
+    int t, n, m, i, x;
+    cin >> t;
+    while (t--) {
+        struct Node* res = NULL;
+        struct Node* first = NULL;
+        struct Node* second = NULL;
+        cin >> n;
+        for (i = 0; i < n; i++) {
+            cin >> x;
+            push(&first, x);
+        }
+        cin >> m;
+        for (i = 0; i < m; i++) {
+            cin >> x;
+            push(&second, x);
+        }
+        res = addTwoLists(first, second);
+        printList(res);
+        if (first) freeList(first);
+        if (second) freeList(second);
+    }
+    return 0;
+}
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+// write a function returns the resultant linked list
+Node* addTwoLists(Node* a, Node* b) {
+    
+    if(a==NULL) return b;
+    if(b==NULL) return a;
+    
+    int sum = a->data + b->data;
+    
+    Node* res = new Node(sum%10);
+    res->next = addTwoLists(a->next,b->next);
+    
+    if(sum>=10)
+    {
+        res->next = addTwoLists(res->next,new Node(sum/10));
+    }
+    return res;
+}
