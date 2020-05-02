8a5c81fcbfe1c6da354bda961909ee7e5228e171
pkunjam <punit17100@iiitnr.edu.in>
2020-05-02 21:48:08 +0530

    btree from array

---
+++ Tree/Construct Binary Tree from Parent Array.cpp
@@ -0,0 +1,137 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+void sort_and_print(vector <int> &v)
+{
+    sort( v.begin() , v.end() );
+    for( int i=0 ; i<v.size() ; i++ )
+        cout<< v[i] << " ";
+    v.clear();
+}
+
+void printLevelOrder(struct Node* root)
+{
+    vector <int> v;
+    queue <Node*> q;
+    
+    q.push(root);
+    
+    Node* next_row = NULL;
+    
+    while(!q.empty())
+    {
+        Node* n = q.front();
+        q.pop();
+        
+        if(n==next_row)
+        {
+            sort_and_print(v);
+            next_row = NULL;
+        }
+        
+        v.push_back(n->data);
+        
+        if(n->left)
+        {
+            q.push(n->left);
+            if(next_row == NULL) next_row = n->left;
+        }
+        
+        if(n->right)
+        {
+            q.push(n->right);
+            if(next_row == NULL) next_row = n->right;
+        }
+    }
+    sort_and_print(v);
+}
+
+Node *createTree(int parent[], int n);
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>> t;
+    while(t--)
+    {
+        int n;
+        cin>> n;
+        int a[n];
+        for( int i=0 ; i<n ; i++ )
+            cin>> a[i];
+        
+        struct Node *res = createTree(a,n);
+        
+        printLevelOrder(res);
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+/*  Function which returns the  root of 
+    the constructed binary tree. */
+
+Node *createTree(int arr[], int n)
+{
+    Node* temp[n];
+    Node* root;
+    
+    for(int i=0;i<n;i++)
+    {
+        temp[i] = new Node(i);
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==-1)
+        {
+            root = temp[i];
+            continue;
+        }
+        
+        Node* parent = temp[arr[i]];
+        
+        if(parent->left==NULL)
+        {
+            parent->left = temp[i];
+        }
+        
+        else
+        {
+            parent->right = temp[i];
+        }
+    }
+    return root;
+}
