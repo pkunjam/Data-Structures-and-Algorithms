88e0b0af7ad368626ed1664cbcc5abdb66a801b4
pkunjam <punit17100@iiitnr.edu.in>
2020-12-11 14:24:06 +0530

    Smallest difference

--- Coding Interview/Smallest difference.cpp
+++ Coding Interview/Smallest difference.cpp
@@ -1,4 +1,4 @@
-// O(N^2) solution
+// O(N^2) time
 
 #include <vector>
 using namespace std;
@@ -25,3 +25,54 @@ vector<int> smallestDifference(vector<int> arrayOne, vector<int> arrayTwo) {
 	
   return {map.first, map.second};
 }
+
+
+// O( nlog(n) + mlog(m)) time and O(1) space
+
+#include <vector>
+using namespace std;
+
+vector<int> smallestDifference(vector<int> arrayOne, vector<int> arrayTwo) {
+  
+	sort(arrayOne.begin(), arrayOne.end());
+	sort(arrayTwo.begin(), arrayTwo.end());
+	
+	int i=0;
+	int j=0;
+	int currDiff = abs(arrayOne[0] - arrayTwo[0]);
+	int minDiff = currDiff;
+	
+	pair<int, int> smallestDiff;
+	
+	while(i < arrayOne.size() && j < arrayTwo.size())
+	{
+		int firstNum = arrayOne[i];
+		int secondNum = arrayTwo[j];
+		
+		if(firstNum < secondNum)
+		{
+			currDiff = abs(firstNum - secondNum);
+			i++;
+		}
+		
+		else if(firstNum > secondNum)
+		{
+			currDiff = abs(firstNum - secondNum);
+			j++;
+		}
+		
+		else
+		{
+			return {firstNum, secondNum};
+		}
+		
+		if(currDiff < minDiff)
+		{
+			minDiff = currDiff;
+		  smallestDiff = make_pair(firstNum, secondNum);
+		}
+	}
+	
+	return {smallestDiff.first, smallestDiff.second};
+}
+
