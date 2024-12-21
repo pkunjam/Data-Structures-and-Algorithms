ea8b340f32222728fe0e6691eb5e6e4f67a60c72
pkunjam <kunjampunit@gmail.com>
2024-12-21 17:07:01 -0500

    Move Zeros

---
+++ Leetcode 75/Move Zeros.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int l = 0;
+        int r = 0;
+
+        while (r < nums.size())
+        {
+            if (nums[r] != 0)
+            {
+                swap(nums[l], nums[r]);
+                l++;
+            }
+
+            r++;
+        }
+    }
+};
