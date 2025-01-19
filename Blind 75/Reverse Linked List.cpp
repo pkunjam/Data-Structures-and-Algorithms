c3834c971dcd7459256303abbf62251f21a23e35
pkunjam <kunjampunit@gmail.com>
2025-01-19 15:37:03 -0500

    Reverse Linked List

---
+++ Blind 75/Reverse Linked List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+          
+          ListNode* p = nullptr;
+          ListNode* n = head;
+
+          while (n != nullptr)
+          {
+            n = head->next;
+            head->next= p;
+            p = head;
+
+            if (n == nullptr) break;
+            
+            head = n;
+          }
+
+          return head;
+    }
+};
